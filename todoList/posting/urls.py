90b5790bb3c27003066b8d3231d17c29f1d4b930
Sephior <k97209735@gmail.com>
2023-08-21 21:24:55 +0900

    수정 및 삭제 서버 연동 완성

--- todoList/posting/urls.py
+++ todoList/posting/urls.py
@@ -5,4 +5,6 @@ from posting import views
 urlpatterns = [
     path('', views.getTaskList, name='getTaskList'),
     path('addTask', views.addTask, name='addTask'),
+    path('update/<int:pk>/<str:work>', views.updateTask, name='updateTask'),
+    path('remove/<int:pk>', views.removeTask, name='removeTask'),
 ]
--- todoList/posting/views.py
+++ todoList/posting/views.py
@@ -6,12 +6,14 @@ from rest_framework.response import Response
 from .models import Task
 # Create your views here.
 
+# get 방식
 @api_view(['GET'])
 def getTaskList(request):
     TasksetQuery = Task.objects.all()
     serializer = TaskSerializer(TasksetQuery, many=True)
     print(serializer.data)
     return Response(serializer.data, status=200)
+
 
 # get 방식이 아니라 Post 방식
 @api_view(['POST'])
@@ -29,3 +31,24 @@ def addTask(request):
             # 아니면 에러 출력
             print(serializer.errors)
     return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def updateTask(request, pk, work):
+    # id로 해당하는 태스크를 가져오기
+    targetTask = Task.objects.get(id=pk)
+    targetTask.work = work
+    targetTask.isComplete = False
+    targetTask.save()
+
+    # targetTask를 직렬화
+    serialized_Task = TaskSerializer(targetTask)
+    return Response(serialized_Task.data, status=200)
+
+
+@api_view(['GET'])
+def removeTask(request, pk):
+    # id로 해당하는 태스크를 가져오기
+    Task.objects.get(id=pk).delete()
+    # 빈 내용이지만 반환하기
+    return Response(status=200)
